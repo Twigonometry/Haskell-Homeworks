b7813039de82427d41bef0e878176b26b272e886
Twigonometry <34944094+Twigonometry@users.noreply.github.com>
2019-10-29 17:23:48 +0000

    TreeInsert method

--- binaryTree.hs
+++ binaryTree.hs
@@ -1,2 +1,11 @@
 module BinaryTree where
-    data BTree a = Leaf a | Node (Tree a) a Node (Tree a)
+    data BTree a = EmptyTree | Node (BTree a) a (BTree a) deriving (Ord, Eq, Show)
+
+    {- compares value of new node x to root
+    if same, don't insert (no duplicates); if smaller, insert into left subtree lSub; if larger, insert into right subtree rSub -}
+    treeInsert :: (Ord a) => a -> BTree a -> BTree a
+    treeInsert x EmptyTree = Node EmptyTree x EmptyTree
+    treeInsert x (Node lSub root rSub)
+        | x == root = Node lSub x rSub
+        | x < root = Node (treeInsert x lSub) root rSub
+        | x > root = Node lSub root (treeInsert x rSub)
