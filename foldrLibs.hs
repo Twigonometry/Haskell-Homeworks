83f5fe369fa30e23b60b56a053ccb8128662e510
Twigonometry <34944094+Twigonometry@users.noreply.github.com>
2019-11-01 20:34:20 +0000

    Completed foldrMap

--- foldrLibs.hs
+++ foldrLibs.hs
@@ -3,8 +3,5 @@ mapFilter :: (a -> b) -> (a -> Bool) -> [a] -> [b]
 mapFilter f p xs = map f (filter p xs)
 
 --redefine map f and filter p using foldr
-{-
-foldMap :: (a -> b) -> [a] -> [b]
-foldMap f (x:xs) = (f x):(foldr f [] xs)
---foldMap f = foldr f []
--}
+foldrMap :: (a -> b) -> [a] -> [b]
+foldrMap f = foldr (\x ys -> f x : ys) []
